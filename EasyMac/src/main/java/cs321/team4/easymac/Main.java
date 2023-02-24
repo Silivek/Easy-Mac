33544de6104ce6aaac320a07d235c3af75700809
Silivek <wkilpatrick90@gmail.com>
2023-02-23 20:49:01 -0600

    moving Main to main package

---
+++ EasyMac/src/main/java/cs321/team4/easymac/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cs321.team4.easymac;
+
+/**
+ *
+ * @author moosi
+ */
+public class Main {
+    /** Generic description here again
+     * 
+     */
+    //TODO try to set up Main class
+    void recordTimeline(){
+        
+    }
+}
