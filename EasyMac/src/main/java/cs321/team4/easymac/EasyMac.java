422407963c53be6dc8e1d9ac0188aef12f6d5023
Silivek <wkilpatrick90@gmail.com>
2023-02-23 19:37:20 -0600

    initial NetBeans project

---
+++ EasyMac/src/main/java/cs321/team4/easymac/EasyMac.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cs321.team4.easymac;
+
+/**
+ *
+ * @author wkilp
+ */
+public class EasyMac {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
