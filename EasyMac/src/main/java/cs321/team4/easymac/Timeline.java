24166429fa7da19e37b28995756a863a21a64cdd
benspurlock <benspurlock@192.168.0.169>
2023-02-24 16:52:28 -0600

    added currentNode, update_current_note()

--- EasyMac/src/main/java/cs321/team4/easymac/Timeline.java
+++ EasyMac/src/main/java/cs321/team4/easymac/Timeline.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cs321.team4.easymac;
+import cs321.team4.easymac.nodes.Node;
 
 /**
  *
@@ -10,4 +11,12 @@ package cs321.team4.easymac;
  */
 public class Timeline {
     //TODO data structure for Timeline
+   Node currentNode;
+   int delayTimer; //Timer for playback
+   
+   void update_current_node() {
+       currentNode = currentNode.getNextNode();
+   }
+   
+   //TODO update comments
 }
