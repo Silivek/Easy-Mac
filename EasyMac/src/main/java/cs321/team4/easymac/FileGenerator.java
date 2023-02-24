1e7300d08f8c27d560ad691d33c6b0483c8899cb
Silivek <wkilpatrick90@gmail.com>
2023-02-23 20:28:43 -0600

    moving File classes to main package

---
+++ EasyMac/src/main/java/cs321/team4/easymac/FileGenerator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cs321.team4.easymac;
+
+/**
+ *
+ * @author icedr
+ */
+public class FileGenerator {
+    public void fileGen() {
+System.out.println("Hello World!"); // TODO begin setting up the file generator classes
+    } 
+}
---
+++ EasyMac/src/main/java/cs321/team4/easymac/FileReader.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cs321.team4.easymac;
+
+/**
+ *
+ * @author icedr
+ */
+public class FileReader {
+    // TODO set up the File Reader classes
+}
