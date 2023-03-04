9ac365078c9ac82b7b8e653daf18662c611a27d3
benspurlock <benspurlock@192.168.0.169>
2023-03-03 19:40:36 -0600

    Added constructors

--- EasyMac/src/main/java/cs321/team4/easymac/nodes/Node.java
+++ EasyMac/src/main/java/cs321/team4/easymac/nodes/Node.java
@@ -12,14 +12,29 @@ public class Node {
     //TODO Node Class
     Node prevNode;
     Node nextNode;
-    int delay; //Delay in milliseconds
+    int delayDuration; //Delay in milliseconds
     
+    //Empty Constructor
+    Node() {};
+    
+    //Main Constructor
+    Node(Node prev, Node next, int delay) {
+        prevNode = prev;
+        nextNode = next;
+        delayDuration = delay;
+    }
+    
+    //Getters
     public Node getPrevNode() {
         return prevNode;
     }
     
     public Node getNextNode() {
         return nextNode;
+    }
+    
+    public Node getDelayDuration() {
+        return delayDuration;
     }
     
     //TODO add setters for node class
