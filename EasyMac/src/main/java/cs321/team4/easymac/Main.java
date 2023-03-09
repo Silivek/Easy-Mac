a1b1e0e3390f3ffcfd165123a35cc35e9c6b9b9e
Melissa Burel <38404360+MelissaBurel@users.noreply.github.com>
2023-03-09 14:33:36 -0600

    refactored for the main class added menu

--- EasyMac/src/main/java/cs321/team4/easymac/Main.java
+++ EasyMac/src/main/java/cs321/team4/easymac/Main.java
@@ -1,110 +1,64 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package cs321.team4.easymac;
 
-import java.nio.file.Path;
-import java.nio.file.Files;
-
+import java.util.Scanner;
 /**
- *
  * @author Melissa
  */
+
 public class Main {
 
-    /**
-     * Generic description here again
-     *
-     */
     //TODO try to set up Main class
 
-    Timeline item;
-    String name;
+     // @param args
+     
+ 
+    public static void main(String args[]) {
+       System.out.println("*********************************************");
+       System.out.println("*                                           *");
+       System.out.println("*             Welcome to EasyMac            *");
+       System.out.println("*                                           *");
+       System.out.println("*    Please make your selection from the    *");
+       System.out.println("*                  menu below               *");
+       System.out.println("*                                           *");
+       System.out.println("*********************************************");
+       
 
-    /**
-     * Takes inputs of the nodes of a timeline and saves the contents to a data
-     * structure?
-     *
-     * @param name
-     */
-    // TODO write the recordTimeline method
-    void recordTimeline(Timeline name) {
-        // save nodes and timings to an array?
+       System.out.println("*********************************************");
+       System.out.println("*                                           *");
+       System.out.println("*            1. Create a new macro          *");
+      // System.out.println("*            2. Load a saved macro          *");
+       System.out.println("*                                           *");
+       System.out.println("*********************************************");
+       
+       // create a scanner object
+       Scanner userSelection = new Scanner(System.in);
+       // prompt user
+       System.out.println("Please make your selection and press enter: ");
+       // store user input
+       int selection = userSelection.nextInt(); 
+       
+       if(selection == 1){
+           // I'm aware this isn't practical, but just wanted something to get testing
+             System.out.println("Enter your desired key strokes, press 3 when finished");
+             Scanner input = new Scanner(System.in);
+             // will inputs be read into node?
+             while(input != 3){
+                int userInput = userSelection.nextInt(); 
+
+                // take user input, create node
+                Node userCreatedNode = getInput(userInput);
+                // add node to timeline
+                // delete node
+                //create a new scanner item
+                Scanner input = new Scanner(System.in);
+                int userInput = userSelection.nextInt();}             
+
+       }
+       
     }
-
-    ;
     
-    /** saveTimline takes a Timeline object and String name
-     * Stores the Timeline Object to the file 
-     * Uses String name to name the file
-     * @param name 
-     */
-    // TODO writed the saveTimeline method
-    void saveTimeline(Timeline item, String name) {
-        
-        // Locate Home Directory for saving the file
-        String userHomeDir = System.getProperty("user.home");
-        
-        // Path of where we would like to store the file
-        Path pathDir = Path.of("${userHomeDir}/EasyMacSavedFiles");
-        
-        // Determine if directory exists/ if not make path with file
-        Boolean exists = Files.exists(pathDir);
-        if (!exists) {
-            // Example from class- not actual path for anything
-            Path newDirectory = Files.createDirectories(pathDir.getParent().resolve("EasyMacSavedFiles/${name}.txt"));
-            newDirectory = Path.createFile(pathDir.getParent().resolve(${name}.txt");
-        }else if{
-            // Directory does exist but file does not
-            // create file on the path
-        }else{
-            // both exist, set path to where the file is
-        }
-            //Path newDirectory = Files.createDirectories(path.getParent().resolve("some/new/dir"));
-
-            //Path pathFile = Path.of("c:/${userHomeDir}/EasyMacSavedFiles/${name}.txt");
-
-            Boolean exists = Files.exists(pathDir);
-            if (!exists) {
-                Path newDirectory = Files.createDirectories(path.getParent().resolve("some/new/dir"));
-                Path newFiles = Files.createFile(newDirectory.resolve("${name}.txt"), attrs);
-                //Path tryAgain = Path.get(System.getProperty("user.home"), "EasyMacSavedFiles");
-
-            }
-            //send contents of timeline to a file
-        };
-        /**
-         * loadTimeline searches the files for a saved timeline and returns its
-         * contents
-         *
-         * @param name
-         * @return
-         */
-
-        // TODO create loadTimeline method
-        Timeline loadTimeline
-        (Timeline name
-        
-            ){
-        // search for name of timeline/name of file
-        // find timeline file and return contents
-         return name;
-        };
-        /**
-         * editTimeline method adds or removes events from the timeline? Will
-         * need to discuss how this is different from Timeline that is already
-         * there
-         *
-         * @param name
-         */
-
-        //TODO created editTimeline method
-        void editTimeline
-        (Timeline name
-        
-        ){
-        // add timeline event
-        // remove timeline event
-    };
-    }
+}
