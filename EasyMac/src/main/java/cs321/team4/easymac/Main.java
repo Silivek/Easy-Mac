34d6a5d844c0de8abecc1082c33398fcac4e1e9d
Melissa Burel <38404360+MelissaBurel@users.noreply.github.com>
2023-03-19 22:33:19 -0500

    Update Main.java
    added in pieces

--- EasyMac/src/main/java/cs321/team4/easymac/Main.java
+++ EasyMac/src/main/java/cs321/team4/easymac/Main.java
@@ -1,53 +1,76 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package cs321.team4.easymac;
 
 import java.util.Scanner;
+
 /**
  * @author Melissa
  */
 
 public class Main {
 
-    //TODO try to set up Main class
+    //TODO write saveTimeline function
+    void saveTimeline(String name, Timeline recordedTimeline) {
+        // write timeline Object to file
+        // use name for name of file
+    }
 
+    ;
+    
+    //TODO write loadTimeline function
+    Timeline loadTimeline(String name) {
+        // find file on file path
+        // return timeline object
+        return Timeline;
+    }
+
+    ;
+    
+    //TODO write editTimeline function
+    Timeline editTimeline(Timeline recordedTimeline) {
+        // find and return timeline
+        // use the Timeline functions to edit timeline
+        // save to tile
+        return recordedTimeline;
+    }
+
+    ;
      // @param args
      
  
     public static void main(String args[]) {
-       System.out.println("*********************************************");
-       System.out.println("*                                           *");
-       System.out.println("*             Welcome to EasyMac            *");
-       System.out.println("*                                           *");
-       System.out.println("*    Please make your selection from the    *");
-       System.out.println("*                  menu below               *");
-       System.out.println("*                                           *");
-       System.out.println("*********************************************");
-       
+        System.out.println("*********************************************");
+        System.out.println("*                                           *");
+        System.out.println("*             Welcome to EasyMac            *");
+        System.out.println("*                                           *");
+        System.out.println("*    Please make your selection from the    *");
+        System.out.println("*                  menu below               *");
+        System.out.println("*                                           *");
+        System.out.println("*********************************************");
 
-       System.out.println("*********************************************");
-       System.out.println("*                                           *");
-       System.out.println("*            1. Create a new macro          *");
-      // System.out.println("*            2. Load a saved macro          *");
-       System.out.println("*                                           *");
-       System.out.println("*********************************************");
-       
-       // create a scanner object
-       Scanner userSelection = new Scanner(System.in);
-       // prompt user
-       System.out.println("Please make your selection and press enter: ");
-       // store user input
-       int selection = userSelection.nextInt(); 
-       
-       if(selection == 1){
-           // I'm aware this isn't practical, but just wanted something to get testing
-             System.out.println("Enter your desired key strokes, press 3 when finished");
-             Scanner input = new Scanner(System.in);
-             // will inputs be read into node?
-             while(input != 3){
-                int userInput = userSelection.nextInt(); 
+        System.out.println("*********************************************");
+        System.out.println("*                                           *");
+        System.out.println("*            1. Create a new macro          *");
+        // System.out.println("*            2. Load a saved macro          *");
+        System.out.println("*                                           *");
+        System.out.println("*********************************************");
+
+        // create a scanner object
+        Scanner userSelection = new Scanner(System.in);
+        // prompt user
+        System.out.println("Please make your selection and press enter: ");
+        // store user input
+        int selection = userSelection.nextInt();
+
+        if (selection == 1) {
+            // I'm aware this isn't practical, but just wanted something to get testing
+            System.out.println("Enter your desired key strokes, press 3 when finished");
+            Scanner input = new Scanner(System.in);
+            int userInput = userSelection.nextInt();            // will inputs be read into node?
+            while (userInput != 3) {
+                // int userInput = userSelection.nextInt(); 
 
                 // take user input, create node
                 Node userCreatedNode = getInput(userInput);
@@ -55,10 +78,8 @@ public class Main {
                 // delete node
                 //create a new scanner item
                 Scanner input = new Scanner(System.in);
-                int userInput = userSelection.nextInt();}             
-
-       }
-       
+                int userInput = userSelection.nextInt();
+            }
+        }
     }
-    
 }
