f84848a5b2b884073ffc58a67a4e2be7aacea8d3
christopherbboggs <christopherbboggs@gmail.com>
2023-03-21 17:17:40 -0500

    New constructors (one for prev node, one for prev & next node.)

--- EasyMac/src/main/java/cs321/team4/easymac/nodes/InputNode.java
+++ EasyMac/src/main/java/cs321/team4/easymac/nodes/InputNode.java
@@ -12,6 +12,12 @@ public class InputNode extends Node{
     boolean pressRelease; //true for press, false for release
     int button; //Keystroke or button press
     
+    public InputNode(Node prev, int delayDuration, boolean pressRelease, int button){
+        super(prev, null, delayDuration);
+        this.pressRelease = pressRelease;
+        this.button = button;
+    }
+    
     public InputNode(Node prev, Node next, int delayDuration, boolean pressRelease, int button){
         super(prev, next, delayDuration);
         this.pressRelease = pressRelease;
