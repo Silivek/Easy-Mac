96c97e7ead5d8a7d25a99102733477d6f7aae6f7
Loganp344 <126211145+Loganp344@users.noreply.github.com>
2023-03-21 18:15:10 -0500

    Created the ReadTimeline function.

--- EasyMac/src/main/java/cs321/team4/easymac/FileReader.java
+++ EasyMac/src/main/java/cs321/team4/easymac/FileReader.java
@@ -3,13 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cs321.team4.easymac;
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.io.ObjectInputStream;
 /**
  *
  * @author Logan Price
  */
 public class FileReader {
     // TODO set up the File Reader classes
+    public void ReadTimeline () throws ClassNotFoundException
+    {
+        try {
+            FileInputStream fIn = new FileInputStream(".Documents/Timeline.txt"); //designates the location to read from
+            try (ObjectInputStream oIn = new ObjectInputStream(fIn)) //creates the object reader
+            {
+                while (oIn.available()!= 0)    //Runs as long as there are objects in the file
+                {
+                  Object tLine = (ObjectInputStream) oIn.readObject();  //set an object equal to a line
+                  System.out.println(tLine); //print the object
+                }
+            } 
+        } catch (IOException ex) {
+        System.out.println("An error occurred.");
+        }
+    }
 }
