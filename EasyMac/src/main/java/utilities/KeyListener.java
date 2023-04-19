4cb832e6f9ff852abea0f7880dc3146bb1443170
Melissa Burel <38404360+MelissaBurel@users.noreply.github.com>
2023-04-18 19:32:59 -0500

    Updated keyListener

--- EasyMac/src/main/java/utilities/KeyListener.java
+++ EasyMac/src/main/java/utilities/KeyListener.java
@@ -4,12 +4,14 @@
  */
 package utilities;
 
+import cs321.team4.easymac.Timeline;
 import cs321.team4.easymac.nodes.KeyInputNode;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
-import cs321.team4.easymac.gui.MainGUI;
+import java.awt.event.MouseEvent;
+import cs321.team4.easymac.nodes.MouseInputNode;
 
 /**
  *
@@ -31,13 +33,39 @@ public class KeyListener implements NativeKeyListener{
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    Timeline userCreatedTimeline = new Timeline();
+    public Timeline getCreatedTimeline(){
+        return userCreatedTimeline;
+    }
+    
     @Override
     public void nativeKeyPressed(NativeKeyEvent e) {
-        int keyCode = e.getKeyCode();
-//        //char vkValueChar = keyPress.toCharArray()[0];
-//        //int vkValueInt = vkValueChar;
-        KeyInputNode nodeToAdd = new KeyInputNode(null, keyCode);
-        //getTimeline().addNode(nodeToAdd);
+        String value = NativeKeyEvent.getKeyText(e.getKeyCode());
+        if (value.equals("left") || value.equals("Left")) {
+                MouseInputNode nodeToAdd = new MouseInputNode(null, MouseEvent.BUTTON1_DOWN_MASK); // left click node
+                userCreatedTimeline.addNode(nodeToAdd);
+            } else if (value.equals("right") || value.equals("Right")) {
+                MouseInputNode nodeToAdd = new MouseInputNode(null, MouseEvent.BUTTON2_DOWN_MASK); // right click node
+                userCreatedTimeline.addNode(nodeToAdd);
+            } else{
+                value = value.toUpperCase();
+                //char ch = e.getKeyChar();
+                //System.out.println("Key char from getKeyChar: " + ch);
+                //int length = value.length();
+                
+               // System.out.println("Length of the string value in KeyListener: " + length);
+              // When converting toCharArray ENTER becomes E
+                char vkValueChar = value.toCharArray()[0];
+               // System.out.println("Value in char array in KeyListener" + vkValueChar);
+                int vkValueInt = vkValueChar;
+               // System.out.println("Integer value from KeyListener: " + vkValueInt);
+                KeyInputNode nodeToAdd = new KeyInputNode(null, vkValueInt);
+                userCreatedTimeline.addNode(nodeToAdd);
+            }
+        //char vkValueChar = keyCode.toCharArray()[0];
+//        int vkValueInt = vkValueChar;
+//        KeyInputNode nodeToAdd = new KeyInputNode(null, keyCode);
+//        userCreatedTimeline.addNode(nodeToAdd);  
         //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
